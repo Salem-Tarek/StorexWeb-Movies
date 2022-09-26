0955eb91a53d6e1a4e8529c8f93d20443ac14f5b
Salem-Tarek <salemtarek224@gmail.com>
2022-09-26 03:45:54 +0200

    Deploying Dist Folder

---
+++ precache-manifest.644361484b5bea148a1d706bfb52976a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "505c633d9934944b8399",
+    "url": "/StorexWeb-Movies/css/app.d0db76fb.css"
+  },
+  {
+    "revision": "18ac710be2b97839c891",
+    "url": "/StorexWeb-Movies/css/chunk-vendors.d49de3d9.css"
+  },
+  {
+    "revision": "36f75f7b805250cdf439630043772a49",
+    "url": "/StorexWeb-Movies/img/poster.36f75f7b.jpg"
+  },
+  {
+    "revision": "fe22cdcff2843dccb0d23f22709f16a8",
+    "url": "/StorexWeb-Movies/index.html"
+  },
+  {
+    "revision": "505c633d9934944b8399",
+    "url": "/StorexWeb-Movies/js/app.4af49c9d.js"
+  },
+  {
+    "revision": "18ac710be2b97839c891",
+    "url": "/StorexWeb-Movies/js/chunk-vendors.14fa962b.js"
+  },
+  {
+    "revision": "75dd1b72289d6ba188cb84db7cfe1172",
+    "url": "/StorexWeb-Movies/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/StorexWeb-Movies/robots.txt"
+  }
+]);
